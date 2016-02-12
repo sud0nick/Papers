f05a90aad0af6dcbf4ec388e6417c33dc10cd3d0
sud0nick <livingxsacrifice@gmail.com>
2016-02-12 17:39:13 -0500

    Added support for both SD and non-SD environments

--- includes/scripts/installDepends.sh
+++ includes/scripts/installDepends.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-
 #  Author: sud0nick
 #  Date:   Jan 2016
 
+RES=$(df | grep sd)
 opkg update > /dev/null;
-opkg install zip > /dev/null;
+
+if [[ -z "$RES" ]]; then
+        opkg install zip > /dev/null;
+else
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/sd/lib:/sd/usr/lib
+        export PATH=$PATH:/sd/usr/bin:/sd/usr/sbin
+        opkg install zip --dest sd > /dev/null;
+fi
+
 echo "Complete"
